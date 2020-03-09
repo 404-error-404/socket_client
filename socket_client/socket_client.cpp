0a87e631a29788e87d19c46280ff077d586de164
404-error-404 <1791781644@qq.com>
2020-03-09 23:16:36 +0800

    添加项目文件。

---
+++ socket_client/socket_client.cpp
@@ -0,0 +1,51 @@
+﻿// socket_client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
+
+int main() {
+    //初始化DLL
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    //创建套接字
+    SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    //向服务器发起请求
+    sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockAddr.sin_port = htons(1234);
+    connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+
+    //接收服务器传回的数据
+    char szBuffer[MAXBYTE] = { 0 };
+    recv(sock, szBuffer, MAXBYTE, NULL);
+
+    //输出接收到的数据
+    printf("Message form server: %s\n", szBuffer);
+
+    //关闭套接字
+    closesocket(sock);
+
+    //终止使用 DLL
+    WSACleanup();
+
+    system("pause");
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
